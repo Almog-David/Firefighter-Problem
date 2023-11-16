bad7f139d2d07082afcf1c4f41f113ec61bacf66
Almog-David <almog.david99@gmail.com>
2023-11-16 14:00:14 +0200

    added function title to utils

---
+++ Utils.py
@@ -0,0 +1,26 @@
+import networkx as nx
+
+def calculate_gamma(graph:nx.DiGraph, source:int, targets:list)-> dict:
+    return 0
+
+def calculate_epsilon(direct_vacinations:dict)->list:
+    return 0
+
+def find_best_direct_vaccination(direct_vacinations:dict, current_time_options:list, targets:list)->tuple:
+    return 0
+
+def spread_virus(graph:nx.DiGraph, infected_nodes:list)->bool:
+    return 0
+
+def spread_vaccination(graph:nx.DiGraph, vaccinated_nodes:list)->None:
+    return
+
+def vaccinate_node(graph:nx.DiGraph, node:int)->None:
+    return
+
+def display_graph(graph:nx.DiGraph)->None:
+    return
+
+
+
+
